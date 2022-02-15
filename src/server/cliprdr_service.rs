rustdesk/rustdesk

windows clipboard
Signed-off-by: fufesou <shuanglongchen@yeah.net>

src/server/cliprdr_service.rs
@@ -1,0 +1,104 @@
+use super::*;
+use clipboard::{create_cliprdr_context, get_rx_client_msg, server_msg, ConnID};
+use hbb_common::{
+    log,
+    tokio::sync::{
+        mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+        Mutex as TokioMutex,
+    },
+    tokio::time::{self, Duration, Instant},
+    ResultType,
+};
+use std::sync::atomic::{AtomicBool, Ordering};
+
+pub use crate::common::CLIPRDR_NAME as NAME;
+
+lazy_static::lazy_static! {
+    static ref MSG_CHANNEL_SERVER: (UnboundedSender<(ConnID, Cliprdr)>, TokioMutex<UnboundedReceiver<(ConnID, Cliprdr)>>) = {
+        let (tx, rx) = unbounded_channel();
+        (tx, TokioMutex::new(rx))
+    };
+}
+
+static RUNNING: AtomicBool = AtomicBool::new(false);
+
+pub fn new() -> GenericService {
+    let sp = GenericService::new(NAME, true);
+    sp.run::<_>(listen::run);
+    sp
+}
+
+pub fn handle_serve_cliprdr_msg(id: i32, msg: Cliprdr) {
+    if RUNNING.load(Ordering::SeqCst) {
+        log::info!("handle handle_serve_cliprdr_msg");
+        MSG_CHANNEL_SERVER
+            .0
+            .send((
+                ConnID {
+                    server_conn_id: id as u32,
+                    remote_conn_id: 0,
+                },
+                msg,
+            ))
+            .unwrap();
+    } else {
+        // should not reach this branch
+    }
+}
+
+mod listen {
+    use super::*;
+
+    static WAIT: Duration = Duration::from_millis(1500);
+
+    #[tokio::main]
+    pub async fn run(sp: GenericService) -> ResultType<()> {
+        let mut cliprdr_context = create_cliprdr_context(false)?;
+
+        RUNNING.store(false, Ordering::SeqCst);
+
+        let mut timer = time::interval_at(Instant::now() + WAIT, WAIT);
+        let mut client_rx = get_rx_client_msg().lock().await;
+        let mut server_rx = MSG_CHANNEL_SERVER.1.lock().await;
+        while sp.ok() {
+            RUNNING.store(true, Ordering::SeqCst);
+
+            tokio::select! {
+                msg = client_rx.recv() => {
+                    match msg {
+                        Some((conn_id, msg)) => {
+                            if conn_id.server_conn_id == 0 {
+                                sp.send(msg)
+                            } else {
+                                sp.send_to(msg, conn_id.server_conn_id as i32)
+                            }
+                        }
+                        None => {
+                            unreachable!()
+                        }
+                    }
+                }
+                msg = server_rx.recv() => {
+                    match msg {
+                        Some((conn_id, msg)) => {
+                            let res = server_msg(&mut cliprdr_context, conn_id, msg);
+                            if res != 0 {
+                                // log::warn!("failed to process message for {}", id);
+                            }
+                        }
+                        None => {
+                            unreachable!()
+                        }
+                    }
+                }
+                _ = timer.tick() => {},
+            }
+            sp.snapshot(|_| Ok(()))?;
+        }
+
+        RUNNING.store(false, Ordering::SeqCst);
+        log::info!("Clipboard listener stopped!");
+
+        Ok(())
+    }
+}

src/ui/remote.rs
@@ -1,6 +1,13 @@
 use crate::client::*;
+#[cfg(not(windows))]
 use crate::common::{
     self, check_clipboard, update_clipboard, ClipboardContext, CLIPBOARD_INTERVAL,
+};
+#[cfg(windows)]
+use crate::common;
+#[cfg(windows)]
+use clipboard::{
+    cliprdr::CliprdrClientContext, create_cliprdr_context, get_rx_client_msg, server_msg, ConnID,
 };
 use enigo::{self, Enigo, KeyboardControllable};
 use hbb_common::{
@@ -1159,6 +1166,16 @@ async fn io_loop(handler: Handler) {
             .as_mut()
             .map(|v| v.render_frame(data).ok());
     });
+
+    #[cfg(windows)]
+    let cliprdr_context = match create_cliprdr_context(false) {
+        Ok(context) => Some(context),
+        Err(err) => {
+            handler.msgbox("error", "Create clipboard error", &err.to_string());
+            None
+        }
+    };
+
     let mut remote = Remote {
         handler,
         video_sender,
@@ -1172,6 +1189,10 @@ async fn io_loop(handler: Handler) {
         timer: time::interval(SEC30),
         last_update_jobs_status: (Instant::now(), Default::default()),
         first_frame: false,
+        #[cfg(windows)]
+        cliprdr_context,
+        #[cfg(windows)]
+        pid: std::process::id(),
     };
     remote.io_loop().await;
 }
@@ -1211,10 +1232,15 @@ struct Remote {
     timer: Interval,
     last_update_jobs_status: (Instant, HashMap<i32, u64>),
     first_frame: bool,
+    #[cfg(windows)]
+    cliprdr_context: Option<Box<CliprdrClientContext>>,
+    #[cfg(windows)]
+    pid: u32,
 }
 
 impl Remote {
     async fn io_loop(&mut self) {
+        #[cfg(not(windows))]
         let stop_clipboard = self.start_clipboard();
         let mut last_recv_time = Instant::now();
         let conn_type = if self.handler.is_file_transfer() {
@@ -1230,6 +1256,13 @@ impl Remote {
                 }
                 self.handler
                     .call("setConnectionType", &make_args!(peer.is_secured(), direct));
+
+                // just build for now
+                #[cfg(not(windows))]
+                let (_client_tx, mut client_rx) = mpsc::unbounded_channel::<i32>();
+                #[cfg(windows)]
+                let mut client_rx = get_rx_client_msg().lock().await;
+
                 loop {
                     tokio::select! {
                         res = peer.next() => {
@@ -1260,6 +1293,21 @@ impl Remote {
                                 }
                             }
                         }
+                        msg = client_rx.recv() => {
+                            #[cfg(not(windows))]
+                            println!("{:?}", msg);
+                            #[cfg(windows)]
+                            match msg {
+                                Some((conn_id, msg)) => {
+                                    if conn_id.remote_conn_id == 0 || conn_id.remote_conn_id == self.pid {
+                                        allow_err!(peer.send(&msg).await);
+                                    }
+                                }
+                                None => {
+                                    unreachable!()
+                                }
+                            }
+                        }
                         _ = self.timer.tick() => {
                             if last_recv_time.elapsed() >= SEC30 {
                                 self.handler.msgbox("error", "Connection Error", "Timeout");
@@ -1284,6 +1332,7 @@ impl Remote {
                     .msgbox("error", "Connection Error", &err.to_string());
             }
         }
+        #[cfg(not(windows))]
         if let Some(stop) = stop_clipboard {
             stop.send(()).ok();
         }
@@ -1321,6 +1370,7 @@ impl Remote {
         }
     }
 
+    #[cfg(not(windows))]
     fn start_clipboard(&mut self) -> Option<std::sync::mpsc::Sender<()>> {
         if self.handler.is_file_transfer() || self.handler.is_port_forward() {
             return None;
@@ -1626,9 +1676,29 @@ impl Remote {
                 Some(message::Union::cursor_position(cp)) => {
                     self.handler.set_cursor_position(cp);
                 }
+                #[allow(unused_variables)]
                 Some(message::Union::clipboard(cb)) => {
+                    #[cfg(not(windows))]
                     if !self.handler.lc.read().unwrap().disable_clipboard {
                         update_clipboard(cb, Some(&self.old_clipboard));
+                    }
+                }
+                #[allow(unused_variables)]
+                Some(message::Union::cliprdr(clip)) => {
+                    log::info!("received cliprdr msg");
+                    #[cfg(windows)]
+                    if !self.handler.lc.read().unwrap().disable_clipboard {
+                        if let Some(context) = &mut self.cliprdr_context {
+                            let res = server_msg(
+                                context,
+                                ConnID {
+                                    server_conn_id: 0,
+                                    remote_conn_id: self.pid,
+                                },
+                                clip,
+                            );
+                            log::info!("server msg returns {}", res);
+                        }
                     }
                 }
                 Some(message::Union::file_response(fr)) => match fr.union {

betaposlab/chainremote

src/server/cliprdr_service.rs
@@ -1,0 +1,104 @@
+use super::*;
+use clipboard::{create_cliprdr_context, get_rx_client_msg, server_msg, ConnID};
+use hbb_common::{
+    log,
+    tokio::sync::{
+        mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+        Mutex as TokioMutex,
+    },
+    tokio::time::{self, Duration, Instant},
+    ResultType,
+};
+use std::sync::atomic::{AtomicBool, Ordering};
+
+pub use crate::common::CLIPRDR_NAME as NAME;
+
+lazy_static::lazy_static! {
+    static ref MSG_CHANNEL_SERVER: (UnboundedSender<(ConnID, Cliprdr)>, TokioMutex<UnboundedReceiver<(ConnID, Cliprdr)>>) = {
+        let (tx, rx) = unbounded_channel();
+        (tx, TokioMutex::new(rx))
+    };
+}
+
+static RUNNING: AtomicBool = AtomicBool::new(false);
+
+pub fn new() -> GenericService {
+    let sp = GenericService::new(NAME, true);
+    sp.run::<_>(listen::run);
+    sp
+}
+
+pub fn handle_serve_cliprdr_msg(id: i32, msg: Cliprdr) {
+    if RUNNING.load(Ordering::SeqCst) {
+        log::debug!("handle handle_serve_cliprdr_msg");
+        MSG_CHANNEL_SERVER
+            .0
+            .send((
+                ConnID {
+                    server_conn_id: id as u32,
+                    remote_conn_id: 0,
+                },
+                msg,
+            ))
+            .unwrap();
+    } else {
+        // should not reach this branch
+    }
+}
+
+mod listen {
+    use super::*;
+
+    static WAIT: Duration = Duration::from_millis(1500);
+
+    #[tokio::main]
+    pub async fn run(sp: GenericService) -> ResultType<()> {
+        let mut cliprdr_context = create_cliprdr_context(true, false)?;
+
+        RUNNING.store(false, Ordering::SeqCst);
+
+        let mut timer = time::interval_at(Instant::now() + WAIT, WAIT);
+        let mut client_rx = get_rx_client_msg().lock().await;
+        let mut server_rx = MSG_CHANNEL_SERVER.1.lock().await;
+        while sp.ok() {
+            RUNNING.store(true, Ordering::SeqCst);
+
+            tokio::select! {
+                msg = client_rx.recv() => {
+                    match msg {
+                        Some((conn_id, msg)) => {
+                            if conn_id.server_conn_id == 0 {
+                                sp.send(msg)
+                            } else {
+                                sp.send_to(msg, conn_id.server_conn_id as i32)
+                            }
+                        }
+                        None => {
+                            unreachable!()
+                        }
+                    }
+                }
+                msg = server_rx.recv() => {
+                    match msg {
+                        Some((conn_id, msg)) => {
+                            let res = server_msg(&mut cliprdr_context, conn_id, msg);
+                            if res != 0 {
+                                // log::warn!("failed to process message for {}", id);
+                            }
+                        }
+                        None => {
+                            unreachable!()
+                        }
+                    }
+                }
+                _ = timer.tick() => {},
+            }
+            sp.snapshot(|_| Ok(()))?;
+        }
+
+        RUNNING.store(false, Ordering::SeqCst);
+        log::info!("Clipboard listener stopped!");
+
+        Ok(())
+    }
+}

src/ui/remote.rs
@@ -2,6 +2,10 @@ use crate::client::*;
 use crate::common::{
     self, check_clipboard, update_clipboard, ClipboardContext, CLIPBOARD_INTERVAL,
 };
+#[cfg(windows)]
+use clipboard::{
+    cliprdr::CliprdrClientContext, create_cliprdr_context, get_rx_client_msg, server_msg, ConnID,
+};
 use enigo::{self, Enigo, KeyboardControllable};
 use hbb_common::{
     allow_err,
@@ -1159,6 +1163,16 @@ async fn io_loop(handler: Handler) {
             .as_mut()
             .map(|v| v.render_frame(data).ok());
     });
+
+    #[cfg(windows)]
+    let cliprdr_context = match create_cliprdr_context(true, false) {
+        Ok(context) => Some(context),
+        Err(err) => {
+            handler.msgbox("error", "Create clipboard error", &err.to_string());
+            None
+        }
+    };
+
     let mut remote = Remote {
         handler,
         video_sender,
@@ -1172,6 +1186,10 @@ async fn io_loop(handler: Handler) {
         timer: time::interval(SEC30),
         last_update_jobs_status: (Instant::now(), Default::default()),
         first_frame: false,
+        #[cfg(windows)]
+        cliprdr_context,
+        #[cfg(windows)]
+        pid: std::process::id(),
     };
     remote.io_loop().await;
 }
@@ -1211,6 +1229,10 @@ struct Remote {
     timer: Interval,
     last_update_jobs_status: (Instant, HashMap<i32, u64>),
     first_frame: bool,
+    #[cfg(windows)]
+    cliprdr_context: Option<Box<CliprdrClientContext>>,
+    #[cfg(windows)]
+    pid: u32,
 }
 
 impl Remote {
@@ -1230,6 +1252,13 @@ impl Remote {
                 }
                 self.handler
                     .call("setConnectionType", &make_args!(peer.is_secured(), direct));
+
+                // just build for now
+                #[cfg(not(windows))]
+                let (_client_tx, mut client_rx) = mpsc::unbounded_channel::<i32>();
+                #[cfg(windows)]
+                let mut client_rx = get_rx_client_msg().lock().await;
+
                 loop {
                     tokio::select! {
                         res = peer.next() => {
@@ -1257,6 +1286,21 @@ impl Remote {
                             if let Some(d) = d {
                                 if !self.handle_msg_from_ui(d, &mut peer).await {
                                     break;
+                                }
+                            }
+                        }
+                        msg = client_rx.recv() => {
+                            #[cfg(not(windows))]
+                            println!("{:?}", msg);
+                            #[cfg(windows)]
+                            match msg {
+                                Some((conn_id, msg)) => {
+                                    if conn_id.remote_conn_id == 0 || conn_id.remote_conn_id == self.pid {
+                                        allow_err!(peer.send(&msg).await);
+                                    }
+                                }
+                                None => {
+                                    unreachable!()
                                 }
                             }
                         }
@@ -1629,6 +1673,24 @@ impl Remote {
                 Some(message::Union::clipboard(cb)) => {
                     if !self.handler.lc.read().unwrap().disable_clipboard {
                         update_clipboard(cb, Some(&self.old_clipboard));
+                    }
+                }
+                #[allow(unused_variables)]
+                Some(message::Union::cliprdr(clip)) => {
+                    log::debug!("received cliprdr msg");
+                    #[cfg(windows)]
+                    if !self.handler.lc.read().unwrap().disable_clipboard {
+                        if let Some(context) = &mut self.cliprdr_context {
+                            let res = server_msg(
+                                context,
+                                ConnID {
+                                    server_conn_id: 0,
+                                    remote_conn_id: self.pid,
+                                },
+                                clip,
+                            );
+                            log::debug!("server msg returns {}", res);
+                        }
                     }
                 }
                 Some(message::Union::file_response(fr)) => match fr.union {